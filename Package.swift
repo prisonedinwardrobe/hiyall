ef56a6521d47151f8e441ea048e95a0a6f9c3418
Leonchik Leonid <34514149+prisonedinwardrobe@users.noreply.github.com>
2018-04-09 17:02:18 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,6 +11,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
     ],
     targets: [
         .target(
